7589d52024292e322ba3072fbfc169c7937e1f9c
juan regonat <juanregonat@gmail.com>
2025-07-20 14:59:21 -0300

    fin video 6 - genericos

---
+++ Orders/Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject]
+        private IRepository repository { get; set; } = null!;
+
+        public List<Country>? Countries { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
