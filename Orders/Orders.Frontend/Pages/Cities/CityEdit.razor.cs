cc1f2ed7b7cefded3c3a8bf448a364fac0f644ca
juan regonat <juanregonat@gmail.com>
2025-08-30 15:30:47 -0300

    fin del crud pais, estado, ciudad. Video 27

---
+++ Orders/Orders.Frontend/Pages/Cities/CityEdit.razor.cs
@@ -0,0 +1,71 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Frontend.Shared;
+using Orders.Shared.Entities;
+using System.Net;
+
+namespace Orders.Frontend.Pages.Cities
+{
+    public partial class CityEdit
+    {
+        private City? city;
+        private FormWithName<City>? cityForm; //es la representación del codigo razor en el codigo c#
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        [Parameter] public int CityId { get; set; }
+
+        protected async override Task OnParametersSetAsync()
+        {
+            var responseHttp = await Repository.GetAsync<City>($"/api/cities/{CityId}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Return();
+                }
+
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            city = responseHttp.Response;
+
+        }
+
+
+        private async Task SaveAsync()
+        {
+            var responseHttp = await Repository.PutAsync("/api/cities", city);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            Return();// si todo fue exitoso, se va a la pagina countries
+
+            //aca agrega una tostada que dice "pais agregado"
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados");
+        }
+
+        private void Return()
+        {
+            cityForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/states/details/{city!.StateId}");
+        }
+    }
+}
---
+++ Orders/Orders.Frontend/Pages/States/StateEdit.razor.cs
@@ -0,0 +1,72 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Frontend.Shared;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.States
+{
+    public partial class StateEdit
+    {
+        private State? state;
+        private FormWithName<State>? stateForm; //es la representación del codigo razor en el codigo c#
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+        //alertas personalizadas
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        // manejo de la navegación: ir de una pagina a otra
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        [EditorRequired, Parameter] public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var responseHttp = await Repository.GetAsync<State>($"/api/states/{Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("/states");
+                }
+                else
+                {
+                    var message = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                }
+                return;
+            }
+            else
+            {
+                state = responseHttp.Response;
+            }
+        }
+        private async Task EditAsync()
+        {
+            var responseHttp = await Repository.PutAsync("/api/states", state);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            Return();// si todo fue exitoso, se va a la pagina countries
+
+            //aca agrega una tostada que dice "agregado"
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados");
+        }
+
+        private void Return()
+        {
+            stateForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/countries/details/{state!.CountryId}"); //https://localhost:7119/countries/details/1
+        }
+    }
+}
