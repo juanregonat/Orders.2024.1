591dc9020c2723b3e645c0f8a0f1e4cafba3fb6f
juan regonat <juanregonat@gmail.com>
2025-10-19 16:13:37 -0300

    se agrego user class

--- Orders/Orders.Shared/Entities/City.cs
+++ Orders/Orders.Shared/Entities/City.cs
@@ -20,6 +20,6 @@ namespace Orders.Shared.Entities
         public int StateId { get; set; }
         public State? State { get; set; }
 
-        
+        public ICollection<User>? Users { get; set; }
     }
 }
